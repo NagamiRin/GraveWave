6708b882b7f29aee00b1da15bbc9511031bc7c39
永見凜 <jijimomo0818@docomo.ne.jp>
2025-09-09 13:35:53 +0900

    GunStatusクラス作成

---
+++ GameFile/Game/src/Actor/Gun/GunStatus.cpp
@@ -0,0 +1,31 @@
+/**
+ * GunStatus.cpp
+ *
+ * 銃のステータス
+ */
+#include "stdafx.h"
+#include "src/Actor/Gun/GunStatus.h"
+
+
+namespace nsApp
+{
+	namespace nsActor
+	{
+		namespace nsGun
+		{
+			GunStatus::GunStatus()
+			{
+			}
+
+
+			GunStatus::~GunStatus()
+			{
+			}
+
+
+			void GunStatus::Setup()
+			{
+			}
+		}
+	}
+}
