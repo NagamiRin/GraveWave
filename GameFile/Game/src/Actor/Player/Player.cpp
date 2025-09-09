19ca5f5d50ff88112d283e04e8a06a05eaec22bc
永見凜 <jijimomo0818@docomo.ne.jp>
2025-09-09 13:52:56 +0900

    Playerクラス作成

---
+++ GameFile/Game/src/Actor/Player/Player.cpp
@@ -0,0 +1,42 @@
+﻿/**
+ * Player.cpp
+ *
+ * プレイヤーのクラス
+ */
+#include "stdafx.h"
+#include "src/Actor/Player/Player.h"
+
+
+namespace nsApp
+{
+	namespace nsActor
+	{
+		namespace nsPlayer
+		{
+			Player::Player()
+			{
+			}
+
+
+			Player::~Player()
+			{
+			}
+
+
+			bool Player::Start()
+			{
+				return true;
+			}
+
+
+			void Player::Update()
+			{
+			}
+
+
+			void Player::Render(RenderContext& rc)
+			{
+			}
+		}		
+	}
+}
---
+++ GameFile/Game/src/Actor/Player/Player.h
@@ -0,0 +1,38 @@
+﻿/**
+ * Player.h
+ * 
+ * プレイヤークラス
+ */
+#pragma once
+#include "src/Actor/CharacterBase.h"
+
+
+namespace nsApp
+{
+	namespace nsActor
+	{
+		namespace nsPlayer
+		{
+			/**
+			 * プレイヤークラス
+			 */
+			class Player : public CharacterBase
+			{
+			public:
+				/** コンストラクタ */
+				Player();
+				/** デストラクタ */
+				~Player();
+
+
+			public:
+				/** 生成時に一度だけ呼ばれる関数。 */
+				bool Start()override;
+				/** 毎フレーム呼ばれる更新処理。 */
+				void Update()override;
+				/** 描画処理。 */
+				void Render(RenderContext& rc)override;
+			};
+		}		
+	}
+}
