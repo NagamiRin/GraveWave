fff563d980bba0ec027d31b0eb291605fd35a9c6
永見凜 <jijimomo0818@docomo.ne.jp>
2025-09-09 14:00:20 +0900

    PlayerStatusクラス作成

---
+++ GameFile/Game/src/Actor/Player/PlayerStatus.cpp
@@ -0,0 +1,31 @@
+﻿/**
+ * PlayerStatus.cpp
+ *
+ * プレイヤーのステータス
+ */
+#include "stdafx.h"
+#include "src/Actor/Player/PlayerStatus.h"
+
+
+namespace nsApp
+{
+	namespace nsActor
+	{
+		namespace nsPlayer
+		{
+			PlayerStatus::PlayerStatus()
+			{
+			}
+
+
+			PlayerStatus::~PlayerStatus()
+			{
+			}
+
+
+			void PlayerStatus::Setup()
+			{
+			}
+		}
+	}
+}
