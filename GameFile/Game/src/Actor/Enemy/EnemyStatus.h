fcfe28c6c108c9075d2d382a5c0a86fba8b79042
永見凜 <jijimomo0818@docomo.ne.jp>
2025-10-22 18:27:51 +0900

    ゾンビのステータスをjsonファイルから読み込むように修正

--- GameFile/Game/src/Actor/Enemy/EnemyStatus.h
+++ GameFile/Game/src/Actor/Enemy/EnemyStatus.h
@@ -18,9 +18,26 @@ namespace nsApp
 			 */
 			class EnemyStatus : public ActorStatus
 			{
-			private:
+			protected:
 				/** 体力 */
 				uint16_t m_hp = 0;
+				/** 最大体力 */  
+				uint16_t m_maxHp = 0;
+				/** 攻撃力 */
+				uint16_t m_attackPower = 0;
+				/** 移動速度 */
+				float m_moveSpeed = 0.0f;
+
+
+			public:
+				/** 移動速度を取得 */
+				inline float GetMoveSpeed() const { return m_moveSpeed; }
+				/** エネミーのHPを取得 */
+				inline uint16_t GetHP() const { return m_hp; }
+				/** エネミーのHPを設定 */
+				inline void const SetHP(uint16_t hp) { m_hp = hp; }
+				/** エネミーのHPをリセット */
+				inline void const ResetHP() { m_hp = m_maxHp; }
 
 
 			public:
---
+++ GameFile/Game/src/Actor/Enemy/ZombieStatus.cpp
@@ -0,0 +1,43 @@
+﻿/**
+ * ZombieStatus.cpp
+ *
+ * ゾンビのステータス
+ */
+#include "stdafx.h"
+#include "src/Actor/Enemy/ZombieStatus.h"
+#include "src/core/ParameterManager.h"
+
+
+namespace nsApp
+{
+	namespace nsActor
+	{
+		namespace nsEnemy
+		{
+			ZombieStatus::ZombieStatus()
+			{
+				ParameterManager::Get().LoadParameter<MasterEnemyStatus>("Assets/Status/Enemy/ZombieStatus.json", [](const nlohmann::json& j, MasterEnemyStatus& p)
+					{
+						p.m_moveSpeed = j["MoveSpeed"].get<float>();
+						p.m_hp = j["HP"].get<uint16_t>();
+						p.m_attackPower = j["AttackPower"].get<uint16_t>();
+					});
+			}
+
+			ZombieStatus::~ZombieStatus()
+			{
+				ParameterManager::Get().UnloadParameter<MasterEnemyStatus>();
+			}
+
+
+			void ZombieStatus::Setup()
+			{
+				auto* parameter = ParameterManager::Get().GetParameter<MasterEnemyStatus>();
+				m_moveSpeed = parameter->m_moveSpeed;
+				m_hp = parameter->m_hp;
+				m_maxHp = parameter->m_hp;
+				m_attackPower = parameter->m_attackPower;
+			}
+		}
+	}
+}
