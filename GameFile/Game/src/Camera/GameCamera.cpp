bb5dfd0f9a48ece13aa4b0f0c0b72d726a606e0c
永見凜 <jijimomo0818@docomo.ne.jp>
2025-11-20 09:20:47 +0900

    ゲームカメラクラスを修正

---
+++ GameFile/Game/src/Camera/GameCamera.cpp
@@ -0,0 +1,97 @@
+﻿/**
+ * GameCamera.cpp
+ *
+ * ゲームカメラクラス
+ */
+#include "stdafx.h"
+#include "GameCamera.h"
+#include "src/core/ParameterManager.h"
+
+
+namespace nsApp
+{  
+    namespace nsCamera
+    {
+        GameCamera::GameCamera()
+        {
+            //パラメーター読み込み
+            ParameterManager::Get().LoadParameter<MasterCameraParameter>("Assets/Parameter/CameraParameter.json", [](const nlohmann::json& j, MasterCameraParameter& p)
+                {
+                    p.m_cameraNear = j["CameraNear"].get<float>();
+                    p.m_cameraFar = j["CameraFar"].get<float>();
+                    p.m_initialCameraPositionX = j["InitialCameraPositionX"].get<float>();
+                    p.m_initialCameraPositionY = j["InitialCameraPositionY"].get<float>();
+                    p.m_initialCameraPositionZ = j["InitialCameraPositionZ"].get<float>();
+                    p.m_initialTargetPositionX = j["InitialTargetPositionX"].get<float>();
+                    p.m_initialTargetPositionY = j["InitialTargetPositionY"].get<float>();
+                    p.m_initialTargetPositionZ = j["InitialTargetPositionZ"].get<float>();
+                    p.m_cameraSensitivity = j["CameraSensitivity"].get<float>();
+                });
+
+            //パラメーター設定
+            auto* parameter = ParameterManager::Get().GetParameter<MasterCameraParameter>();
+            m_cameraNear = parameter->m_cameraNear;
+            m_cameraFar = parameter->m_cameraFar;
+            m_initialCameraPositionX = parameter->m_initialCameraPositionX;
+            m_initialCameraPositionY = parameter->m_initialCameraPositionY;
+            m_initialCameraPositionZ = parameter->m_initialCameraPositionZ;
+            m_initialTargetPositionX = parameter->m_initialTargetPositionX;
+            m_initialTargetPositionY = parameter->m_initialTargetPositionY;
+            m_initialTargetPositionZ = parameter->m_initialTargetPositionZ;
+            m_cameraSensitivity = parameter->m_cameraSensitivity;
+
+            /** 近平面を設定 */
+            g_camera3D->SetNear(m_cameraNear);
+            /** 遠平面を設定 */
+            g_camera3D->SetFar(m_cameraFar);
+
+            // @TODO：　テスト用
+            m_cameraPos = Vector3(m_initialCameraPositionX, m_initialCameraPositionY, -m_initialCameraPositionZ);
+            m_targetPos = Vector3(m_initialTargetPositionX, m_initialTargetPositionY, m_initialTargetPositionZ);
+            m_cameraDir = Vector3::Front;
+        }
+
+
+        GameCamera::~GameCamera()
+        {
+            ParameterManager::Get().UnloadParameter<MasterCameraParameter>();
+        }
+
+
+        bool GameCamera::Start()
+        {
+            return true;
+        }
+
+
+        void GameCamera::Update()
+        {
+            SetCameraTarget();
+            CameraUpdate();
+        }
+
+
+        void GameCamera::SetCameraTarget()
+        {
+            m_targetPos.x = m_cameraPos.x + (m_cameraDir.x * 10.0f);
+            m_targetPos.y = m_cameraPos.y + (m_cameraDir.y * 10.0f);
+            m_targetPos.z = m_cameraPos.z + (m_cameraDir.z * 10.0f);
+        }
+
+
+        void GameCamera::CameraUpdate()
+        {
+            /** カメラ座標を更新 */
+            g_camera3D->SetPosition(m_cameraPos);
+            //* 注視点を更新 */
+            g_camera3D->SetTarget(m_targetPos);
+            /** カメラを更新 */
+            g_camera3D->Update();
+        }
+
+
+        void GameCamera::Render(RenderContext& rc)
+        {
+        }
+    }
+}
---
+++ GameFile/Game/src/Camera/GameCamera.h
@@ -0,0 +1,89 @@
+﻿/**
+ * GameCamera.h
+ *
+ * ゲームカメラクラス
+ */
+#pragma once
+
+
+namespace nsApp
+{
+	namespace nsCamera
+	{
+		class ActorStatus;
+		class Player;
+
+		/**
+		 *ゲームオブジェクトの基底クラス
+		 */
+		class GameCamera : public IGameObject
+		{
+		private:
+			/** カメラ注視点を設定 */
+			void SetCameraTarget();
+			/** カメラ更新 */
+			void CameraUpdate();
+
+
+		private:
+			/** カメラ座標 */
+			Vector3 m_cameraPos = Vector3::Zero;
+			/** 注視点 */
+			Vector3 m_targetPos = Vector3::Zero;
+			/** カメラの向き */
+			Vector3 m_cameraDir = Vector3::Zero;
+
+
+		private:
+			/** 近平面 */
+			float m_cameraNear = 0.0f;
+			/** 遠平面 */
+			float m_cameraFar = 0.0f;
+			/** カメラ座標の初期値 */
+			float m_initialCameraPositionX = 0.0f;
+			float m_initialCameraPositionY = 0.0f;
+			float m_initialCameraPositionZ = 0.0f;
+			/** 注視点の初期値 */
+			float m_initialTargetPositionX = 0.0f;
+			float m_initialTargetPositionY = 0.0f;
+			float m_initialTargetPositionZ = 0.0f;
+			/** 垂直方向のカメラ画角限界値 */
+			float m_verticalLimitAngle = 0.0f;
+			/** 水平方向のカメラが書く限界値 */
+			float m_horizontalLimitAngle = 0.0f;
+			/** カメラ感度 */
+			float m_cameraSensitivity = 0.0f;
+
+
+		public:
+			/**
+			 * コンストラクタ
+			 *
+			 * playerのポインタを受け取る
+			 */
+			GameCamera();
+			/** デストラクタ */
+			virtual ~GameCamera();
+
+
+		public:
+			/** オブジェクト生成時に一度だけ実行される関数 */
+			bool Start()override;
+			/** 毎フレーム呼び出される関数 */
+			void Update()override;
+			/** モデルの描画処理を行う関数 */
+			void Render(RenderContext& rc)override;
+
+
+		public:
+			/** カメラ座標を設定 */
+			const void SetCameraPos(const Vector3& pos) { m_cameraPos = pos; };
+			/** カメラ注視点を設定 */
+			const void SetTargetPos(const Vector3& pos) { m_targetPos = pos; };
+			/** カメラの向きを設定 */
+			const void SetCameraDir(const Vector3& dir) { m_cameraDir = dir; };
+		};
+	}
+}
+
+
