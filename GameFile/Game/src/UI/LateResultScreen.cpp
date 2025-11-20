491f474b819b3f942a17b62f8baaee0b307d6053
永見凜 <jijimomo0818@docomo.ne.jp>
2025-11-20 10:29:06 +0900

    リザルトのスコア表示を実装

--- GameFile/Game/src/UI/LateResultScreen.cpp
+++ GameFile/Game/src/UI/LateResultScreen.cpp
@@ -6,6 +6,7 @@
 #include "stdafx.h"
 #include "LateResultScreen.h"
 #include "src/UI/UIBase.h"
+#include "src/Core/SaveData.h"
 
 
 namespace nsApp
@@ -28,12 +29,14 @@ namespace nsApp
             m_uiCanvas = std::make_unique<UICanvas>();
             m_uiCanvas->m_transform.m_localPosition = Vector3::Zero;
 
-            m_scoreUI = m_uiCanvas->CreateUI<StringUI>();
-            m_scoreUI->Initialize(L"スコア：0", 1.0f, Vector3::Zero, Vector4::White, Vector2::Zero, 0.0f);
-
             //リザルトの画像を生成
             auto* titleImage = m_uiCanvas->CreateUI<ImageUI>();
             titleImage->Initialize("Assets/Image/GameOverScreen.DDS", 1920.0f, 1080.0f, Vector3::Zero, Vector3::One, Quaternion::Identity);
+
+            //最終スコアを表示
+            auto* finalScore = m_uiCanvas->CreateUI<NumberUI>();
+            finalScore->Initialize("Assets/UI/Numbers", nsCore::SaveData::Get().GetScore(), 20.0f, 45.0f, Vector3(-900.0f, 150.0f, 0.0f), Vector3::One, Quaternion::Identity);
+            finalScore->SetColor(Vector4::Black);
 
             return true;
         }
