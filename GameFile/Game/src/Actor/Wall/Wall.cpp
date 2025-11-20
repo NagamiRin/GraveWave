e77effb79a8c96239d913b4d4422fb6bcc01f0be
永見凜 <jijimomo0818@docomo.ne.jp>
2025-11-20 09:18:13 +0900

    防壁クラスを作成

---
+++ GameFile/Game/src/Actor/Wall/Wall.cpp
@@ -0,0 +1,59 @@
+﻿/**
+ * Wall.cpp
+ *
+ * 防壁クラス
+ */
+#include "stdafx.h"
+#include "Wall.h"
+#include "src/Core/ParameterManager.h"
+
+
+namespace nsApp
+{
+	namespace nsActor
+	{
+		namespace nsWall
+		{
+			Wall::Wall()
+			{
+				ParameterManager::Get().LoadParameter<MasterWallParameter>("Assets/Parameter/WallParameter.json", [](const nlohmann::json& j, MasterWallParameter& p)
+					{
+						p.m_maxDurability = j["MaxDurability"].get<uint16_t>();
+					});
+
+				auto* param = ParameterManager::Get().GetParameter<MasterWallParameter>();
+				m_maxDurability = param->m_maxDurability;
+				m_durability = param->m_maxDurability;
+			}
+
+
+			Wall::~Wall()
+			{
+				//ステータス削除
+				delete m_status;
+				m_status = nullptr;				
+			}
+
+
+			bool Wall::Start()
+			{				
+				m_model.Init("Assets/ModelData/Wall/Wall.tkm");
+				m_model.SetPosition(Vector3::Zero);
+
+				return true;
+			}
+
+
+			void Wall::Update()
+			{
+				SuperClass::Update();				
+			}
+
+
+			void Wall::Render(RenderContext& rc)
+			{
+				m_model.Draw(rc);
+			}			
+		}
+	}
+}
