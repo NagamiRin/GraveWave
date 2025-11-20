fadd184ff2643db98bfc6f6f7eec53fbea357d42
永見凜 <jijimomo0818@docomo.ne.jp>
2025-11-20 20:35:53 +0900

    残弾アイコンを追加

--- GameFile/Game/src/UI/RemainingBulletsUI.cpp
+++ GameFile/Game/src/UI/RemainingBulletsUI.cpp
@@ -8,6 +8,11 @@
 #include "src/UI/UIBase.h"
 
 
+namespace
+{
+    constexpr float canvasWideSize = 548.0f;
+}
+
 namespace nsApp
 {
     namespace nsUI
@@ -31,7 +36,7 @@ namespace nsApp
             //残弾数UIを生成
             //背景
             auto* backImage = m_uiCanvas->CreateUI<ImageUI>();
-            backImage->Initialize("Assets/UI/RemainingBulletsUI/Back.DDS", 548.0f, 219.0f, Vector3::Zero, Vector3::One, Quaternion::Identity);
+            backImage->Initialize("Assets/UI/RemainingBulletsUI/Back.DDS", canvasWideSize, 219.0f, Vector3::Zero, Vector3::One, Quaternion::Identity);
 
             //残弾数の文字列
             m_ammoUI = m_uiCanvas->CreateUI<NumberUI>();
@@ -46,6 +51,11 @@ namespace nsApp
         {
             m_ammoUI->NumberUpdate("Assets/UI/Numbers", m_remainingAmmo, 30.0f, 50.0f);
 
+            if (m_bulletsIconList.size() != m_maxAmmo) {
+                /*if (m_bulletsIconList.size() > m_maxAmmo)m_bulletsIconList.pop_back();
+                if (m_bulletsIconList.size() > m_maxAmmo)m_bulletsIconList.pop_back();*/
+            }
+
             m_uiCanvas->Update();
         }
 
@@ -53,6 +63,16 @@ namespace nsApp
         void RemainingBulletsUI::Render(RenderContext& rc)
         {
             m_uiCanvas->Render(rc);
+        }
+
+
+        void RemainingBulletsUI::DecidePlace()
+        {
+            constexpr float maxWideSize = 50.0f;
+            constexpr float verticalRatio = 2.0f;
+
+            //弾アイコンを横に並べる
+
         }
     }
 }
