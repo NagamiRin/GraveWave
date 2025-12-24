8e413a0aa13fbfa6e8e7a390b2c44a2d7e7508ea
永見凜 <jijimomo0818@docomo.ne.jp>
2025-12-24 18:08:17 +0900

    防壁HPバー修正

--- GameFile/Game/src/UI/WallHPUI.cpp
+++ GameFile/Game/src/UI/WallHPUI.cpp
@@ -8,6 +8,14 @@
 #include "src/UI/UIBase.h"
 
 
+namespace {
+    constexpr float YELLOW_RATE = 0.5f;
+    constexpr float RAD_RATE = 0.2f;
+    constexpr float NUMBER_WIDE_SIZE = 18.0f;
+    constexpr float NUMBER_VERTICAL_SIZE = 32.0f;
+}
+
+
 namespace nsApp
 {
     namespace nsUI {
@@ -27,17 +35,22 @@ namespace nsApp
         {
             //キャンバスを生成
             m_uiCanvas = std::make_unique<UICanvas>();
-            m_uiCanvas->m_transform.m_localPosition = Vector3(0.0f, 470.0f, 0.0f);
+            m_uiCanvas->m_transform.m_localPosition = Vector3(0.0f, 0.0f, 0.0f);
+                        
+            //HPバーの背景
+            auto* backImage = m_uiCanvas->CreateUI<ImageUI>();
+            backImage->Initialize("Assets/UI/WallHP/HPBarBack.dds", 1920.0f, 1080.0f, Vector3::Zero, Vector3::One, Quaternion::Identity);
+
+            //耐久度の％表示
+            m_rate = m_uiCanvas->CreateUI<NumberUI>();
+            m_rate->Initialize("Assets/UI/Numbers/Classic/", 0, NUMBER_WIDE_SIZE, NUMBER_VERTICAL_SIZE, Vector3(60.0f, -425.0f, 0.0f), Vector3::One, Quaternion::Identity);
+            m_rate->SetColor(Vector4(0.0f, 0.5f, 0.0f, 1.0f));
 
             //HPバーの本体
             m_HPBar = m_uiCanvas->CreateUI<ImageUI>();
-            m_HPBar->Initialize("Assets/UI/WallHP/HPBar.dds", 1100.0f, 58.0f, Vector3(-550.0f, 0.5f, 0.0f), Vector3::One, Quaternion::Identity);
+            m_HPBar->Initialize("Assets/UI/WallHP/HPBar.dds", 645.0f, 28.0f, Vector3(-324.0f, -456.0f, 0.0f), Vector3::One, Quaternion::Identity);
             m_HPBar->SetPivot(Vector2(0.0f, 0.5f));
-            m_HPBar->SetMulColor(Vector4::Red);
-
-            //HPバーの背景
-            auto* backImage = m_uiCanvas->CreateUI<ImageUI>();
-            backImage->Initialize("Assets/UI/WallHP/HPBarBack.dds", 1122.0f, 71.0f, Vector3::Zero, Vector3::One, Quaternion::Identity);
+            m_HPBar->SetMulColor(Vector4(0.0f, 0.5f, 0.0f, 1.0f));
 
             m_uiCanvas->Update();
 
@@ -47,18 +60,23 @@ namespace nsApp
 
         void WallHPUI::Update()
         {
-            m_uiCanvas->Update();
-
-            /*if (m_maxDurability <= 0.0f) {
-                return;
-            }*/
-
+            //HPバーのサイズをかえる
             const float hp = static_cast<float>(m_durability);
             const float maxHp = static_cast<float>(m_maxDurability);
 
             const float wightScale = hp / maxHp;
             m_HPBar->SetSize(wightScale, 1.0f);
 
+            //HPバーの色を変える
+            const float rate = hp / maxHp;
+            if (rate <= RAD_RATE)m_HPBar->SetMulColor(Vector4(0.7f, 0.2f, 0.2f, 1.0f));
+            else if (rate <= YELLOW_RATE) m_HPBar->SetMulColor(Vector4(0.7f, 0.7f, 0.1f, 1.0f));
+
+            //％表示をかえる
+            m_rate->NumberUpdate("Assets/UI/Numbers/Classic/", static_cast<uint8_t>(rate * 100), NUMBER_WIDE_SIZE, NUMBER_VERTICAL_SIZE);
+            m_rate->SetColor(Vector4(0.0f, 0.5f, 0.0f, 1.0f));
+
+            m_uiCanvas->Update();
         }
 
 
