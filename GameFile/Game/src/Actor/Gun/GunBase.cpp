8c7b5381c7e0b55182959608c09743c2f7724b65
永見凜 <jijimomo0818@docomo.ne.jp>
2025-12-27 18:03:57 +0900

    ADSを実装

--- GameFile/Game/src/Actor/Gun/GunBase.cpp
+++ GameFile/Game/src/Actor/Gun/GunBase.cpp
@@ -43,13 +43,70 @@ namespace nsApp
 
 				if (m_currentReloadTime >= 0.0f && m_isReloading)ReloadAnimation();
 
+				OnADS();
+				m_recoilSystem->Update();
+
 				m_transform.m_localPosition = m_offsetPosition + m_adjustPosition;
+
+				// @todo for test
+				// 銃を反動に応じて上に向かせたい
+				{
+					Quaternion t = Quaternion::Identity;
+					//Vector3 recoilDirection = Vector3(m_recoilSystem->GetRecoil().x, m_recoilSystem->GetRecoil().y, 0.0f);
+					//if (recoilDirection.Length() > 0.001f) {
+					//	recoilDirection *= 0.1f;
+					//	recoilDirection += m_direction;
+					//	// 1. 前方ベクトル(Z)の正規化
+					//	Vector3 zAxis = recoilDirection;
+					//	zAxis.Normalize();
+
+					//	// 2. 右ベクトル(X)の算出 (Y軸(0,1,0) と Z軸の外積)
+					//	// 注意: 方向が真上や真下に近いと外積がゼロになり計算不能になるため、
+					//	// 実戦ではここで「ZとUpが平行でないか」のチェックを入れるのが安全です。
+					//	Vector3 xAxis = Vector3::Up;
+					//	xAxis.Cross(recoilDirection);
+					//	xAxis.Normalize();
+					//	// 3. 真の上ベクトル(Y)の算出 (Z軸 と X軸の外積)
+					//	// 既に直交しているので正規化は理論上不要だが、誤差対策ですることもある
+					//	Vector3 yAxis = zAxis;
+					//	yAxis.Cross(xAxis);
+
+					//	// 4. 回転行列の構築
+					//	// DirectXは行優先(Row-Major)か列優先(Column-Major)かによりますが、
+					//	// XMMATRIXは通常、基底ベクトルを行にセットします。
+					//	Matrix rotMatrix = Matrix::Identity;
+					//	memcpy(&rotMatrix.m[0], &xAxis, sizeof(xAxis)); // X軸
+					//	memcpy(&rotMatrix.m[1], &yAxis, sizeof(yAxis)); // Y軸
+					//	memcpy(&rotMatrix.m[2], &zAxis, sizeof(zAxis)); // Z軸
+
+					//	t.SetRotation(rotMatrix);
+					//}
+					m_transform.m_localRotation = t;
+				}
+
 				m_transform.UpdateTransform();
 
 				m_model.SetPosition(m_transform.m_position);
 				m_model.SetRotation(m_transform.m_rotation);
 				m_model.SetScale(m_transform.m_localScale);
 				m_model.Update();
+			}
+
+
+			void GunBase::OnADS()
+			{
+				m_currentADSTime += g_gameTime->GetFrameDeltaTime();
+
+				float t = m_currentADSTime / m_ADSSpeed;
+
+				if (t >= 1.0f) {
+					t = 0.0f;
+				}
+
+				// 目的位置
+				Vector3 targetPos = m_isADS ? m_ADSFirePosition : m_hipFirePosition;
+
+				m_offsetPosition = Math::Lerp<Vector3>(t, m_prevPosition, targetPos);
 			}
 
 
